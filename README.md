ce0c6583ac32eaad5141ef5358017a31c0fa3911
dnJJ-tensor <taichihirose@TaichinoMacBook-Pro.local>
2020-08-02 06:17:13 +0900

    my first commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-#diffusion eq
+# diffusion eq
+This program was created to simulate two deminsional heat diffusion phenomena.
+
+## Overview
+### outline of this program
+This program was created to simulate two deminsional heat diffusion phenomena by compiling python(pandas DataFrame, numpy matplotlib) and Fortran90/95. Input, Output and visualization of data is executed by Python. Fortran subroutine undertakes simulation itself.  
+### detail of simulation 
+Euler Explict method is adopted for this Numerical simulation. First of all, main.py reads csv-file which approximate initial conditon as pandas DataFrame. Second, this DataFrame is converted to Numpy 2dimentional array and pass it to Fortran subroutine(hereinafter, this is called "heat_Euler"). Before passing array to subroutine, main.py will check whether sets of parametes meet convergence condition or not. After that, heat_Euler do calculation designated time which is decided by varuable itrnum. Finally, Python functions, Conter and SurFace visualize result of simulation. Please always check numerical answer which is computed by heat_Euler because heat_Euler won't check convergence of numerical answer.
+
+## Requirement
+macOS Catalina 10.15.6
+gfortran 10.1.10
+python 3.8.5
+Environments under Windows 10 pro is tested.
+
+## contact
+My twitter account : @dnJJ_tensor
+I long to get feedback from anyone because I am a not professional engineer. I'm a university student, so There are a lot of things I must learn.
--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import japanize_matplotlib
 import heat_Euler
 
 
-#三次元プロットをする関数
-def SurFace(l1, l2, n1, n2, z, max, mid, min, ttl):
+#function which make 2Dconter map
+def Conter(l1, l2, n1, n2, z, max, mid, min, ttl):
     x1 = np.linspace(0, l1, n1)
     x2 = np.linspace(0, l2, n2)
  
@@ -15,30 +15,46 @@ def SurFace(l1, l2, n1, n2, z, max, mid, min, ttl):
 
     fig = plt.figure()   
 
-    #
     # meshgrid で作った X と Y、そして高さ Z を contour に渡す
-    #contourなら等高線、contourfなら塗り潰し
+
     plt.contourf(X1, X2, z, cmap = 'jet')
     
-    ##カラーバー
-    pp=plt.colorbar (ticks=[min, mid, max], orientation="vertical") # カラーバーの表示 
-    pp.set_label("Label", fontname="Arial", fontsize=24) #カラーバーのラベル
-
     #以下のコマンドを追加すると、アスペクト比が揃う
     #plt.gca().set_aspect('equal')
     plt.title(ttl)
     plt.show()
     #結果を画像保存するコマンド
     #resultという名前のフォルダに画像を保存ズル
-    fig.savefig("result/T_in{}step.png".format(ttl))
+    fig.savefig("result/T_in{}step_2d.png".format(ttl))
     
     return  
+
+#function which make 3d figure
+def SurFace(l1, l2, n1, n2, z, max, mid, min, ttl):
+    #make grid
+    x1 = np.linspace(0, l1, n1)
+    x2 = np.linspace(0, l2, n2)
+ 
+    X1, X2 = np.meshgrid(x1, x2)
+
+    fig = plt.figure() 
+
+    ax = fig.add_subplot(111, projection='3d')
+
+    ax.set_title("3d plot of {} times iteration".format(ttl))
+
+    ax.plot_surface(X1, X2, z, cmap = 'jet')
+
+    plt.show()
+    fig.savefig("result/T_in{}step_3d.png".format(ttl))
+
+    return
 
 
 
 #CSVデータをpandasDataFrameとして読み込み
 #fileの名前
-filename = 'Tinit_test1.csv'
+filename = 'Tinit_test.csv'
 df_Tinit = pd.read_csv("data/{}".format(filename), header = None )
 
 
@@ -69,16 +85,15 @@ dy = ly / (ny - 1)
 DX = kappa*dt/(dx**2)
 DY = kappa*dt/(dy**2)
 if (DX + DY <= 0.50):
-    #イテレーションの回数
-    itrmax = 20
-    for itr in range(1, itrmax+1):
-        Tinout = heat_Euler.twodheat_euler(Tinout, DX, DY, nx, ny)
+    #number of iteration
+    itrnum = 300
+    for itr in range(1, itrnum+1):
+        Tinout = heat_Euler.twodscalor_euler_explc(Tinout, DX, DY, nx, ny)
         
-    SurFace(lx,ly, nx, ny, Tinout, bar_max, bar_mid, bar_min,'{}'.format(itrmax))
-            
-    #SurFace(lx,ly, nx, ny, Tinout, bar_max, bar_mid, bar_min,'{}'.format(itrmax))
+    Conter(lx,ly, nx, ny, Tinout, bar_max, bar_mid, bar_min,'{}'.format(itrnum))
+    SurFace(lx,ly, nx, ny, Tinout, bar_max, bar_mid, bar_min,'{}'.format(itrnum))
     #分布図を保存
-    np.savetxt('data/Result_in_{}step.csv'.format(itrmax),Tinout,delimiter=',')
+    np.savetxt('data/Result_in_{}step.csv'.format(itrnum),Tinout,delimiter=',')
 
 #パラメータが収束条件を満たさないとき  
 else :
